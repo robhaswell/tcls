6e5360d57062cb139ca712929126b4525df84f54
Rob Haswell <me@robhaswell.co.uk>
2016-04-28 14:23:39 +0100

    Half a producer test. Testing this stuff is trickier than I expected and I have run out of time today.

--- input.go
+++ input.go
@@ -1,10 +1,30 @@
 package main
 
+import (
+	"encoding/json"
+	"github.com/adjust/rmq"
+)
+
 type ConnectionProducer struct {
 	previous []Connection
 }
 
 func (producer *ConnectionProducer) Report(connections []Connection) error {
-	// Calculate new connections and report them.
+	/*
+	Report all new connections to the queue.
+
+	Any errors which occur will result in the error being returned. It is the
+	responsibility of the caller to log or otherwise handle this error.
+	*/
+	connection := rmq.OpenConnection("tcls", "tcp", "localhost:6379", 0)
+	taskQueue := connection.OpenQueue("connections")
+
+	for connection := range(connections) {
+		taskBytes, err := json.Marshal(connection)
+		if err != nil {
+			return err
+		}
+		taskQueue.PublishBytes(taskBytes)
+	}
 	return nil
 }
